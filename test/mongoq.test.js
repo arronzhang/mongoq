087c93cb55d226b4a29bf738fb53b8d4e99e2342
Hidden <zzdhidden@gmail.com>
2011-09-11 03:04:54 +0800

    Migrate db.test

--- test/mongoq.test.js
+++ test/mongoq.test.js
@@ -1,47 +1,9 @@
 
-/**
-* Module dependencies.
-*/
 var mongoq = require('mongoq')
 , should = require('should');
 
 module.exports = {
 	'test .version': function(){
 		mongoq.version.should.match(/^\d+\.\d+\.\d+$/);
-	}
-	, 'test db arguments': function(){
-		var db = mongoq("mongodb:\/\/fred:foobar@localhost:27017/testdb?auto_reconnect=true&poolSize=2");
-		var options = db.options;
-		options.should.be.a('object');
-		options.auto_reconnect.should.be.true;
-		options.poolSize.should.equal(2);
-		options.host.should.equal("localhost");
-		options.port.should.eql("27017");
-		options.username.should.eql("fred");
-		options.password.should.eql("foobar");
-		db.name.should.eql("testdb");
-
-		db = mongoq("mongodb:\/\/127.0.0.1:27018/testdb?auto_reconnect");
-		options = db.options;
-		options.should.be.a('object');
-		options.auto_reconnect.should.be.true;
-		options.host.should.equal("127.0.0.1");
-		options.port.should.eql("27018");
-		should.strictEqual(options.username, undefined);
-
-		db = mongoq("mongodb:\/\/127.0.0.1:27018/testdb?auto_reconnect=false");
-		options = db.options;
-		options.should.be.a('object');
-		options.auto_reconnect.should.be.false;
-
-		db = mongoq("testdb", {auto_reconnect: true});
-		options = db.options;
-		options.should.be.a('object');
-		options.host.should.equal("127.0.0.1");
-		options.port.should.eql("27017");
-		should.strictEqual(options.username, undefined);
-		options.auto_reconnect.should.be.true;
-		should.strictEqual(options.poolSize, undefined);
-		db.name.should.eql("testdb");
 	}
 };
