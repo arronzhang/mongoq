e2ad0db731f26e92c9020c0f220eadef0e776f77
Hidden <zzdhidden@gmail.com>
2011-09-15 00:15:55 +0800

    Util parseArgs

---
+++ test/util.test.js
@@ -0,0 +1,27 @@
+
+var mongoq = require('mongoq')
+, util = mongoq.util
+, should = require('should');
+
+module.exports = {
+	'test util parseArgs': function(){
+		(function() {
+			var args = util.parseArgs(arguments);
+			args.all.should.have.length(3);
+			args.clean.should.have.length(3);
+			args.clean[0].should.be.equal(1);
+			should.not.exist(args.callback);
+
+		})(1,2,3);
+
+		(function() {
+			var args = util.parseArgs(arguments);
+			args.all.should.have.length(4);
+			args.clean.should.have.length(3);
+			args.clean[0].should.be.equal(1);
+			should.exist(args.callback);
+			args.callback.should.have.an.instanceof(Function);
+		})(1,2,3, function() {
+		});
+	}
+};
