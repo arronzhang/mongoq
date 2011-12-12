04def1f4d2bed15c2a55f9797ec339e6578a7163
Hidden <zzdhidden@gmail.com>
2011-12-12 18:41:48 +0800

    Update

--- examples/users.js
+++ examples/users.js
@@ -13,13 +13,16 @@ User.remove() //clear test date
 		User.insert( [{ _id: 1, name: "Jack", age: 22 }, { _id: 2, name: "Lucy", age: 20 }] ) //Add Jack and Lucy
 			.and( User.insert( { _id: 3, name: "Mike", age: 21 } ) ) //Add Mike synchronous
 			.and( function(u1, u2) {
+				return User.update({_id: 3}, {$set: {age: 25}}, {safe: 1});
+			} )
+			.and( function(u1, u2, u3) {
 				// Will find after add Jack, Lucy and Mike
 				return User.findOne( { name: u2[0]["name"] } )
 			} )
-			.done( function(u1, u2, u3) { //All be done
+			.done( function(u1, u2, u3, u4) { //All be done
 				assert.deepEqual( u1, [{ _id: 1, name: "Jack", age: 22 }, { _id: 2, name: "Lucy", age: 20 }], "Insert first" );
 				assert.deepEqual( u2, [{ _id: 3, name: "Mike", age: 21 }], "insert second" );
-				assert.deepEqual( u3, { _id: 3, name: "Mike", age: 21 }, "Find after insert" );
+				assert.deepEqual( u4, { _id: 3, name: "Mike", age: 25 }, "Find after insert" );
 				db.close();
 			} )
 			.fail( function( err ) { // Any error occur
